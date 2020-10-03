25f0c6e4d3d1cfd4fdaad19347f6d2bcf90ac367
龚宇 <564542431@qq.com>
2020-10-03 20:56:29 +0800

    基础框架

--- MyUniqueBox/Universal/Managers/SQL/MUBSQLiteManager.m
+++ MyUniqueBox/Universal/Managers/SQL/MUBSQLiteManager.m
@@ -40,7 +40,7 @@
     for (NSString *memberID in memberIDs) {
         __block NSInteger count = 0;
         [self.queue inDatabase:^(FMDatabase * _Nonnull db) {
-            FMResultSet *rs = [db executeQuery:@"select count(member_id) from pixivFollowingUser where member_id = ?", @(memberID.integerValue)];
+            FMResultSet *rs = [db executeQuery:@"select count(member_id) from MUBPixivFollowingUser where member_id = ?", @(memberID.integerValue)];
             while ([rs next]) {
                 count = [rs intForColumnIndex:0];
             }
@@ -65,7 +65,7 @@
     for (NSString *memberID in memberIDs) {
         __block NSInteger count = 0;
         [self.queue inDatabase:^(FMDatabase * _Nonnull db) {
-            NSString *query = [NSString stringWithFormat:@"select count(member_id) from pixivBlockUser where member_id = %@ and block_level %@ %ld", memberID, isEqual ? @"=" : @"!=", blockLevel];
+            NSString *query = [NSString stringWithFormat:@"select count(member_id) from MUBPixivBlockUser where member_id = %@ and block_level %@ %ld", memberID, isEqual ? @"=" : @"!=", blockLevel];
             FMResultSet *rs = [db executeQuery:query];
             while ([rs next]) {
                 count = [rs intForColumnIndex:0];
@@ -85,7 +85,7 @@
     for (NSString *memberID in memberIDs) {
         __block NSInteger count = 0;
         [self.queue inDatabase:^(FMDatabase * _Nonnull db) {
-            FMResultSet *rs = [db executeQuery:@"select count(member_id) from pixivBlockUser where member_id = ?", @(memberID.integerValue)];
+            FMResultSet *rs = [db executeQuery:@"select count(member_id) from MUBPixivBlockUser where member_id = ?", @(memberID.integerValue)];
             while ([rs next]) {
                 count = [rs intForColumnIndex:0];
             }
@@ -103,7 +103,7 @@
     for (NSString *memberID in memberIDs) {
         __block NSInteger blockLevel = -100; // -100 表示没有从数据库中查找到数据
         [self.queue inDatabase:^(FMDatabase * _Nonnull db) {
-            FMResultSet *rs = [db executeQuery:@"select block_level from pixivBlockUser where member_id = ?", @(memberID.integerValue)];
+            FMResultSet *rs = [db executeQuery:@"select block_level from MUBPixivBlockUser where member_id = ?", @(memberID.integerValue)];
             while ([rs next]) {
                 blockLevel = [rs intForColumnIndex:0];
             }
@@ -113,21 +113,21 @@
         // 如果数据表中没有这个人的记录，那么添加一条记录；如果有记录，并且 block_level 不是 1，即便是 2，也修改成 1
         if (blockLevel == -100) {
             [self.queue inDatabase:^(FMDatabase * _Nonnull db) {
-                BOOL success = [db executeUpdate:@"INSERT INTO pixivBlockUser (id, member_id, user_name, block_level) values(?, ?, ?, ?)", NULL, @(memberID.integerValue), NULL, @(1)];
+                BOOL success = [db executeUpdate:@"INSERT INTO MUBPixivBlockUser (id, member_id, user_name, block_level) values(?, ?, ?, ?)", NULL, @(memberID.integerValue), NULL, @(1)];
                 if (success) {
                     [[MUBLogManager defaultManager] addDefaultLogWithFormat:@"Pixiv userId: %ld 已添加状态: 确定拉黑", memberID.integerValue];
                 } else {
-                    [[MUBLogManager defaultManager] addErrorLogWithFormat:@"往数据表:pixivBlockUser中插入数据时发生错误：%@", [db lastErrorMessage]];
+                    [[MUBLogManager defaultManager] addErrorLogWithFormat:@"往数据表:MUBPixivBlockUser中插入数据时发生错误：%@", [db lastErrorMessage]];
                     [[MUBLogManager defaultManager] addErrorLogWithFormat:@"数据：userId: %ld", memberID.integerValue];
                 }
             }];
         } else if (blockLevel != 1) {
             [self.queue inDatabase:^(FMDatabase * _Nonnull db) {
-                BOOL success = [db executeUpdate:@"UPDATE pixivBlockUser SET block_level = 1 WHERE member_id = ?", @(memberID.integerValue)];
+                BOOL success = [db executeUpdate:@"UPDATE MUBPixivBlockUser SET block_level = 1 WHERE member_id = ?", @(memberID.integerValue)];
                 if (success) {
                     [[MUBLogManager defaultManager] addDefaultLogWithFormat:@"Pixiv userId: %ld 已更新状态: 确定拉黑", memberID.integerValue];
                 } else {
-                    [[MUBLogManager defaultManager] addErrorLogWithFormat:@"往数据表:pixivBlockUser中更新数据时发生错误：%@", [db lastErrorMessage]];
+                    [[MUBLogManager defaultManager] addErrorLogWithFormat:@"往数据表:MUBPixivBlockUser中更新数据时发生错误：%@", [db lastErrorMessage]];
                     [[MUBLogManager defaultManager] addErrorLogWithFormat:@"数据：%@", @{@"userId": @(memberID.integerValue), @"blockLevel": @(blockLevel)}];
                 }
             }];
@@ -141,7 +141,7 @@
     for (NSString *memberID in memberIDs) {
         __block NSInteger blockLevel = -100; // -100 表示没有从数据库中查找到数据
         [self.queue inDatabase:^(FMDatabase * _Nonnull db) {
-            FMResultSet *rs = [db executeQuery:@"select block_level from pixivBlockUser where member_id = ?", @(memberID.integerValue)];
+            FMResultSet *rs = [db executeQuery:@"select block_level from MUBPixivBlockUser where member_id = ?", @(memberID.integerValue)];
             while ([rs next]) {
                 blockLevel = [rs intForColumnIndex:0];
             }
@@ -151,21 +151,21 @@
         // 如果数据表中没有这个人的记录，那么添加一条记录；如果有记录，并且 block_level 不是 2，即便是 1，也修改成 2
         if (blockLevel == -100) {
             [self.queue inDatabase:^(FMDatabase * _Nonnull db) {
-                BOOL success = [db executeUpdate:@"INSERT INTO pixivBlockUser (id, member_id, user_name, block_level) values(?, ?, ?, ?)", NULL, @(memberID.integerValue), NULL, @(2)];
+                BOOL success = [db executeUpdate:@"INSERT INTO MUBPixivBlockUser (id, member_id, user_name, block_level) values(?, ?, ?, ?)", NULL, @(memberID.integerValue), NULL, @(2)];
                 if (success) {
                     [[MUBLogManager defaultManager] addDefaultLogWithFormat:@"Pixiv userId: %ld 已添加状态: 不确定拉黑", memberID.integerValue];
                 } else {
-                    [[MUBLogManager defaultManager] addErrorLogWithFormat:@"往数据表:pixivBlockUser中插入数据时发生错误：%@", [db lastErrorMessage]];
+                    [[MUBLogManager defaultManager] addErrorLogWithFormat:@"往数据表:MUBPixivBlockUser中插入数据时发生错误：%@", [db lastErrorMessage]];
                     [[MUBLogManager defaultManager] addErrorLogWithFormat:@"数据：userId: %ld", memberID.integerValue];
                 }
             }];
         } else if (blockLevel != 2) {
             [self.queue inDatabase:^(FMDatabase * _Nonnull db) {
-                BOOL success = [db executeUpdate:@"UPDATE pixivBlockUser SET block_level = 2 WHERE member_id = ?", @(memberID.integerValue)];
+                BOOL success = [db executeUpdate:@"UPDATE MUBPixivBlockUser SET block_level = 2 WHERE member_id = ?", @(memberID.integerValue)];
                 if (success) {
                     [[MUBLogManager defaultManager] addDefaultLogWithFormat:@"Pixiv userId: %ld 已更新状态: 不确定拉黑", memberID.integerValue];
                 } else {
-                    [[MUBLogManager defaultManager] addErrorLogWithFormat:@"往数据表:pixivBlockUser中更新数据时发生错误：%@", [db lastErrorMessage]];
+                    [[MUBLogManager defaultManager] addErrorLogWithFormat:@"往数据表:MUBPixivBlockUser中更新数据时发生错误：%@", [db lastErrorMessage]];
                     [[MUBLogManager defaultManager] addErrorLogWithFormat:@"数据：%@", @{@"userId": @(memberID.integerValue), @"blockLevel": @(blockLevel)}];
                 }
             }];
@@ -263,14 +263,14 @@
     NSMutableArray *follows = [NSMutableArray array];
     NSMutableArray *blocks = [NSMutableArray array];
     [self.queue inDatabase:^(FMDatabase * _Nonnull db) {
-        FMResultSet *rs = [db executeQuery:@"select member_id from pixivFollowingUser"];
+        FMResultSet *rs = [db executeQuery:@"select member_id from MUBPixivFollowingUser"];
         while ([rs next]) {
             [follows addObject:@([rs intForColumnIndex:0])];
         }
         [rs close];
     }];
     [self.queue inDatabase:^(FMDatabase * _Nonnull db) {
-        FMResultSet *rs = [db executeQuery:@"select member_id from pixivBlockUser"];
+        FMResultSet *rs = [db executeQuery:@"select member_id from MUBPixivBlockUser"];
         while ([rs next]) {
             [blocks addObject:@([rs intForColumnIndex:0])];
         }
@@ -287,7 +287,7 @@
 - (BOOL)isWeiboStatusExistsWithStatusId:(NSString *)statusId {
     NSMutableArray *result = [NSMutableArray array];
     [self.queue inDatabase:^(FMDatabase * _Nonnull db) {
-        FMResultSet *rs = [db executeQuery:@"select * from weiboStatus where weibo_id = ?", statusId];
+        FMResultSet *rs = [db executeQuery:@"select * from MUBWeiboDownloadStatus where weibo_id = ?", statusId];
         while ([rs next]) {
             [result addObject:[rs stringForColumn:@"weibo_id"]];
         }
@@ -300,24 +300,24 @@
 - (void)insertWeiboStatuses:(NSArray<MUBResourceWeiboStatusModel *> *)models {
     for (MUBResourceWeiboStatusModel *model in models) {
         [self.queue inDatabase:^(FMDatabase * _Nonnull db) {
-            NSString *update = @"INSERT INTO weiboStatus (id, weibo_id, author_id, author_name, text, publish_time, fetch_time) values(?, ?, ?, ?, ?, ?, ?)";
+            NSString *update = @"INSERT INTO MUBWeiboDownloadStatus (id, weibo_id, author_id, author_name, text, publish_time, fetch_time) values(?, ?, ?, ?, ?, ?, ?)";
             NSArray *arguments = @[[NSNull null], model.idstr, model.user.idstr, model.user.screenName, model.text, model.createdAtSqliteStr, [[NSDate date] formattedDateWithFormat:MUBTimeFormatyMdHms]];
             
             BOOL success = [db executeUpdate:update withArgumentsInArray:arguments];
             if (!success) {
-                [[MUBLogManager defaultManager] addErrorLogWithFormat:@"往数据表:weiboStatus中插入数据时发生错误：%@", [db lastErrorMessage]];
+                [[MUBLogManager defaultManager] addErrorLogWithFormat:@"往数据表:MUBWeiboDownloadStatus中插入数据时发生错误：%@", [db lastErrorMessage]];
                 [[MUBLogManager defaultManager] addErrorLogWithFormat:@"插入的数据：%@", model];
             }
         }];
         
         for (NSString *imgURL in model.imgUrls) {
             [self.queue inDatabase:^(FMDatabase * _Nonnull db) {
-                NSString *update = @"INSERT INTO weiboImage (id, weibo_id, image_url) values(?, ?, ?)";
+                NSString *update = @"INSERT INTO MUBWeiboDownloadImage (id, weibo_id, image_url) values(?, ?, ?)";
                 NSArray *arguments = @[[NSNull null], model.idstr, imgURL];
                 
                 BOOL success = [db executeUpdate:update withArgumentsInArray:arguments];
                 if (!success) {
-                    [[MUBLogManager defaultManager] addErrorLogWithFormat:@"往数据表:weiboImage中插入数据时发生错误：%@", [db lastErrorMessage]];
+                    [[MUBLogManager defaultManager] addErrorLogWithFormat:@"往数据表:MUBWeiboDownloadImage中插入数据时发生错误：%@", [db lastErrorMessage]];
                     [[MUBLogManager defaultManager] addErrorLogWithFormat:@"插入的数据：%@", imgURL];
                 }
             }];
@@ -338,7 +338,7 @@
 //    }
 //    //判断数据库是否已经打开，如果没有打开，提示失败
 //    if (![db open]) {
-//        [[MRBLogManager defaultManager] showLogWithFormat:@"往数据表:weiboUsers中插入数据时发生错误：%@", [db lastErrorMessage]];
+//        [[MRBLogManager defaultManager] showLogWithFormat:@"往数据表:MUBWeiboUser中插入数据时发生错误：%@", [db lastErrorMessage]];
 //        
 //        return;
 //    }
@@ -351,9 +351,9 @@
 //        for (NSInteger i = 0; i < weiboUsers.count; i++) {
 //            NSDictionary *weiboUser = weiboUsers[i];
 //            
-//            BOOL weiboUserSuccess = [db executeUpdate:@"INSERT INTO weiboUsers (id, screen_name, user_url, add_time, status) values(?, ?, ?, ?, ?)", NULL, weiboUser[@"screenName"], weiboUser[@"userUrl"], [[NSDate date] formattedDateWithFormat:@"yyyy-MM-dd HH:mm:ss"], @(status)];
+//            BOOL weiboUserSuccess = [db executeUpdate:@"INSERT INTO MUBWeiboUser (id, screen_name, user_url, add_time, status) values(?, ?, ?, ?, ?)", NULL, weiboUser[@"screenName"], weiboUser[@"userUrl"], [[NSDate date] formattedDateWithFormat:@"yyyy-MM-dd HH:mm:ss"], @(status)];
 //            if (!weiboUserSuccess) {
-//                [[MRBLogManager defaultManager] showLogWithFormat:@"往数据表:weiboUsers中插入数据时发生错误：%@", [db lastErrorMessage]];
+//                [[MRBLogManager defaultManager] showLogWithFormat:@"往数据表:MUBWeiboUser中插入数据时发生错误：%@", [db lastErrorMessage]];
 //                [[MRBLogManager defaultManager] showLogWithFormat:@"数据：%@", weiboUser];
 //            }
 //        }
@@ -375,7 +375,7 @@
 //    }
 //    //判断数据库是否已经打开，如果没有打开，提示失败
 //    if (![db open]) {
-//        [[MRBLogManager defaultManager] showLogWithFormat:@"往数据表:weiboUsers中插入数据时发生错误：%@", [db lastErrorMessage]];
+//        [[MRBLogManager defaultManager] showLogWithFormat:@"往数据表:MUBWeiboUser中插入数据时发生错误：%@", [db lastErrorMessage]];
 //        
 //        return;
 //    }
@@ -388,10 +388,10 @@
 //        for (NSInteger i = 0; i < weiboUserScreenNames.count; i++) {
 //            NSString *weiboUserScreenName = weiboUserScreenNames[i];
 //            
-//            NSString *sqliteStr = [NSString stringWithFormat:@"UPDATE weiboUsers SET status = %ld WHERE screen_name = %@", status, weiboUserScreenName];
+//            NSString *sqliteStr = [NSString stringWithFormat:@"UPDATE MUBWeiboUser SET status = %ld WHERE screen_name = %@", status, weiboUserScreenName];
 //            BOOL weiboUserSuccess = [db executeUpdate:sqliteStr];
 //            if (!weiboUserSuccess) {
-//                [[MRBLogManager defaultManager] showLogWithFormat:@"往数据表:weiboUsers中插入数据时发生错误：%@", [db lastErrorMessage]];
+//                [[MRBLogManager defaultManager] showLogWithFormat:@"往数据表:MUBWeiboUser中插入数据时发生错误：%@", [db lastErrorMessage]];
 //                [[MRBLogManager defaultManager] showLogWithFormat:@"数据：%@", weiboUserScreenName];
 //            }
 //        }
@@ -415,7 +415,7 @@
 //    }
 //    //判断数据库是否已经打开，如果没有打开，提示失败
 //    if (![db open]) {
-//        [[MRBLogManager defaultManager] showLogWithFormat:@"往数据表:weiboRecommendArtists中插入数据时发生错误：%@", [db lastErrorMessage]];
+//        [[MRBLogManager defaultManager] showLogWithFormat:@"往数据表:MUBWeiboRecommendArtist中插入数据时发生错误：%@", [db lastErrorMessage]];
 //        
 //        return;
 //    }
@@ -433,9 +433,9 @@
 //                url = [NSString stringWithFormat:@"https://twitter.com/%@", site.allValues[0]];
 //            }
 //            
-//            BOOL success = [db executeUpdate:@"INSERT INTO weiboRecommendArtists (id, weiboUserId, weiboUser, weiboStatus, recommendSite, recommendAccount, recommendUrl, recordTime) values(?, ?, ?, ?, ?, ?, ?, ?)", NULL, model.user_id_str, model.user_screen_name, model.text, site.allKeys[0], site.allValues[0], url, [[NSDate date] formattedDateWithFormat:@"yyyy-MM-dd HH:mm:ss"]];
+//            BOOL success = [db executeUpdate:@"INSERT INTO MUBWeiboRecommendArtist (id, weiboUserId, weiboUser, weiboStatus, recommendSite, recommendAccount, recommendUrl, recordTime) values(?, ?, ?, ?, ?, ?, ?, ?)", NULL, model.user_id_str, model.user_screen_name, model.text, site.allKeys[0], site.allValues[0], url, [[NSDate date] formattedDateWithFormat:@"yyyy-MM-dd HH:mm:ss"]];
 //            if (!success) {
-//                [[MRBLogManager defaultManager] showLogWithFormat:@"往数据表:weiboRecommendArtists中插入数据时发生错误：%@", [db lastErrorMessage]];
+//                [[MRBLogManager defaultManager] showLogWithFormat:@"往数据表:MUBWeiboRecommendArtist中插入数据时发生错误：%@", [db lastErrorMessage]];
 //                [[MRBLogManager defaultManager] showLogWithFormat:@"数据：%@", model];
 //            }
 //        }
@@ -457,7 +457,7 @@
 //    }
 //    //判断数据库是否已经打开，如果没有打开，提示失败
 //    if (![db open]) {
-//        [[MRBLogManager defaultManager] showLogWithFormat:@"从数据表:weiboRecommendArtists中查询数据时发生错误：%@", [db lastErrorMessage]];
+//        [[MRBLogManager defaultManager] showLogWithFormat:@"从数据表:MUBWeiboRecommendArtist中查询数据时发生错误：%@", [db lastErrorMessage]];
 //        [[MRBLogManager defaultManager] showLogWithFormat:@"数据：%@", model];
 //        
 //        return NO;
@@ -474,7 +474,7 @@
 //            break;
 //        }
 //        
-//        FMResultSet *rs = [db executeQuery:@"select * from weiboRecommendArtists where recommendSite = ? COLLATE NOCASE and recommendAccount = ? COLLATE NOCASE", site.allKeys[0], site.allValues[0]];
+//        FMResultSet *rs = [db executeQuery:@"select * from MUBWeiboRecommendArtist where recommendSite = ? COLLATE NOCASE and recommendAccount = ? COLLATE NOCASE", site.allKeys[0], site.allValues[0]];
 //        while ([rs next]) {
 //            foundCount += 1;
 //        }
